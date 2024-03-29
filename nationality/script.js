65bd189e5c7b531b96af42e2de19a762538eb548
VanshMotwani <vsh24.7.2003@gmail.com>
2024-03-29 18:31:22 +0530

    Done maybe finally?

--- nationality/script.js
+++ nationality/script.js
@@ -7,17 +7,17 @@ function loadData(year,continent) {
       d3.json('data/country_codes_mapping.json'), // country code to make it easy to load svg for flags
       d3.json('data/continent-country.json') // continent to country from the csv file , preprocessed for reducing redundant computation
    ]).then(function([data, countryCodes,contientmap]) {
-      d3.select('svg').selectAll('*').remove(); // this removes the previous plot 
-   
-   
+      d3.select('svg').selectAll('*').remove(); // this removes the previous plot
+
+
       const countries_con= contientmap[continent]; // gets the array of country for a given continent , returns a list of all contries if it all
-      
+
       // filters the data from the data file based on continent
       let filteredData = data.filter(d => countries_con.includes(d.country));
 
       // sets margin for svg
       const margin = { top: 20, right: 30, bottom: 30, left: 40 };
-      
+
       const svg = d3.select("svg"),
          width = +svg.attr("width") - margin.left - margin.right,
          height = +svg.attr("height") - margin.top - margin.bottom;
@@ -37,51 +37,52 @@ function loadData(year,continent) {
       const g = svg.append("g")
          .attr("transform", `translate(${margin.left},${margin.top})`);
 
-      
+
       // Plot the position for the X and Y axis
       g.append("g")
           .attr("transform", `translate(0,${height})`)
           .call(d3.axisBottom(x));
-  
+
       g.append("g")
           .call(d3.axisLeft(y).ticks(null, ".1s"));
 
       // the flag data is in country codes , hence we translate it using the file with the translation present
       filteredData.forEach(d => {
          if (d.country in countryCodes) {
-             d.countryCode = countryCodes[d.country];
+             d.countryCode = countryCodes[d.country].toLowerCase();
+             console.log(d.countryCode);
          } else {
              console.warn(`No code found for country: ${d.country}`);
              d.countryCode = "";
             }
       });
-   
+
       // Append flag images for each data point
       g.selectAll(".flag")
         .data(filteredData)
         .enter().append("image")
         .attr("class", "flag")
-        .attr("xlink:href", d => `flag_png/${d.countryCode}.png`) // getting the correct flag based on country code 
+        .attr("xlink:href", d => `flags/${d.countryCode}.svg`) // getting the correct flag based on country code
         .attr("width", 20) // flag width
         .attr("height", 20) // flag height
         // used square for asthetics and minimising obstruction
 
          // another visual feature that ensures the point is on the centre of the flag and not the corner , to make reading easier
-        .attr("x", d => x(d.average) - 10) 
-        .attr("y", d => y(d.total) - 10) 
+        .attr("x", d => x(d.average) - 10)
+        .attr("y", d => y(d.total) - 10)
         .on("mouseover", function(event, d) {
            // adding tooltip that gives extra information on hover
            d3.select("#tooltip")
                  .style("visibility", "visible")
                  .html(`Country: ${d.country}<br>Average: ${d.average.toFixed(2)}<br>Total: ${d.total.toFixed(2)}`)
-                 .style("left", (event.pageX + 10) + "px") 
-                 .style("top", (event.pageY - 10) + "px"); 
+                 .style("left", (event.pageX + 10) + "px")
+                 .style("top", (event.pageY - 10) + "px");
         })
         .on("mouseout", function() {
            // removing the tooltip
            d3.select("#tooltip").style("visibility", "hidden");
         });
-      
+
    });
 }
 
@@ -100,7 +101,5 @@ document.getElementById('yearSelector').addEventListener('change', function() {
 document.getElementById('continentSelector').addEventListener('change', function() {
     const selectedYear = document.getElementById('yearSelector').value; // similar to above listener fetches year
     const selectedContinent = this.value; // new continent
-    loadData(selectedYear, selectedContinent); 
+    loadData(selectedYear, selectedContinent);
 });
-
-
